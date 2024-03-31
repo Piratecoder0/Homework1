188b0b68f8d06d402683e2180de4ffb79013edf6
Piratecoder0 <a0905364368@gmail.com>
2024-03-31 12:39:38 +0800

    Initial

--- hw1/src/LiaoToken/LiaoToken.sol
+++ hw1/src/LiaoToken/LiaoToken.sol
@@ -1,4 +1,95 @@
 // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.0;
+
+// interface IERC20 {
+//     function totalSupply() external view returns (uint256);
+//     function balanceOf(address account) external view returns (uint256);
+//     function transfer(address to, uint256 amount) external returns (bool);
+//     function transferFrom(address from, address to, uint256 value) external returns (bool);
+//     function approve(address spender, uint256 amount) external returns (bool);
+//     function allowance(address owner, address spender) external view returns (uint256);
+
+//     event Transfer(address indexed from, address indexed to, uint256 value);
+//     event Approval(address indexed owner, address indexed spender, uint256 value);
+// }
+
+// contract LiaoToken is IERC20 {
+//     mapping(address => mapping(address => uint256)) private _allowances;
+//     mapping(address account => uint256) private _balances;
+//     mapping(address account => bool) isClaim;
+
+//     uint256 private _totalSupply;
+//     string private _name;
+//     string private _symbol;
+
+//     event Claim(address indexed user, uint256 indexed amount);
+
+//     constructor(string memory name_, string memory symbol_) payable {
+//         _name = name_;
+//         _symbol = symbol_;
+//     }
+
+//     function decimals() public pure returns (uint8) {
+//         return 18;
+//     }
+
+//     function name() public view returns (string memory) {
+//         return _name;
+//     }
+
+//     function symbol() public view returns (string memory) {
+//         return _symbol;
+//     }
+
+//     function totalSupply() external view returns (uint256) {
+//         return _totalSupply;
+//     }
+
+//     function balanceOf(address account) external view returns (uint256) {
+//         return _balances[account];
+//     }
+
+//     function claim() external returns (bool) {
+//         if (isClaim[msg.sender]) revert();
+//         _balances[msg.sender] += 1 ether;
+//         _totalSupply += 1 ether;
+//         emit Claim(msg.sender, 1 ether);
+//         return true;
+//     }
+
+//     function transfer(address to, uint256 amount) external returns (bool) {
+//         require(_balances[msg.sender] >= amount, "Insufficient balance");
+
+//         _balances[msg.sender] -= amount;
+//         _balances[to] += amount;
+
+//         emit Transfer(msg.sender, to, amount);
+//         return true;
+//     }
+
+//     function transferFrom(address from, address to, uint256 value) external returns (bool) {
+//         require(_balances[from] >= value, "Insufficient balance");
+//         require(value <= _allowances[from][msg.sender], "Allowance exceeded");
+
+//         _balances[from] -= value;
+//         _balances[to] += value;
+//         _allowances[from][msg.sender] -= value;
+
+//         emit Transfer(from, to, value);
+//         return true;
+//     }
+
+//     function approve(address spender, uint256 amount) external returns (bool) {
+//         _allowances[msg.sender][spender] = amount;
+//         emit Approval(msg.sender, spender, amount);
+//         return true;
+//     }
+
+//     function allowance(address owner, address spender) public view returns (uint256) {
+//         return _allowances[owner][spender];
+//     }
+// }
+
 pragma solidity ^0.8.0;
 
 interface IERC20 {
@@ -14,7 +105,7 @@ interface IERC20 {
 }
 
 contract LiaoToken is IERC20 {
-    // TODO: you might need to declare several state variable here
+    mapping(address => mapping(address => uint256)) private _allowances;
     mapping(address account => uint256) private _balances;
     mapping(address account => bool) isClaim;
 
@@ -59,18 +150,34 @@ contract LiaoToken is IERC20 {
     }
 
     function transfer(address to, uint256 amount) external returns (bool) {
-        // TODO: please add your implementaiton here
+        require(_balances[msg.sender] >= amount, "Insufficient balance");
+
+        _balances[msg.sender] -= amount;
+        _balances[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
+        return true;
     }
 
     function transferFrom(address from, address to, uint256 value) external returns (bool) {
-        // TODO: please add your implementaiton here
+        require(_balances[from] >= value, "Insufficient balance");
+        require(value <= _allowances[from][msg.sender], "Allowance exceeded");
+
+        _balances[from] -= value;
+        _balances[to] += value;
+        _allowances[from][msg.sender] -= value;
+
+        emit Transfer(from, to, value);
+        return true;
     }
 
     function approve(address spender, uint256 amount) external returns (bool) {
-        // TODO: please add your implementaiton here
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
     }
 
     function allowance(address owner, address spender) public view returns (uint256) {
-        // TODO: please add your implementaiton here
+        return _allowances[owner][spender];
     }
 }
--- hw1/src/NFinTech/NFinTech.sol
+++ hw1/src/NFinTech/NFinTech.sol
@@ -25,6 +25,9 @@ interface IERC721TokenReceiver {
 
 contract NFinTech is IERC721 {
     // Note: I have declared all variables you need to complete this challenge
+    error ERC721InvalidOperator(address operator);
+    error ERC721IncorrectOwner(address from, uint256 tokenId, address previousOwner);
+
     string private _name;
     string private _symbol;
 
@@ -76,29 +79,79 @@ contract NFinTech is IERC721 {
 
     function setApprovalForAll(address operator, bool approved) external {
         // TODO: please add your implementaiton here
+        if (operator == address(0)){
+            revert ERC721InvalidOperator(operator);
+        }
+        _operatorApproval[msg.sender][operator] = approved;
+
+        emit ApprovalForAll(msg.sender, operator, approved);
     }
 
     function isApprovedForAll(address owner, address operator) public view returns (bool) {
         // TODO: please add your implementaiton here
+        return _operatorApproval[owner][operator];
     }
 
     function approve(address to, uint256 tokenId) external {
         // TODO: please add your implementaiton here
+        address owner = _owner[tokenId];
+
+        require(msg.sender == owner || isApprovedForAll(owner, msg.sender), "Not Authorized");
+
+        _tokenApproval[tokenId] = to;
+
+        emit Approval(owner, to , tokenId);
     }
 
     function getApproved(uint256 tokenId) public view returns (address operator) {
         // TODO: please add your implementaiton here
+        require(_owner[tokenId] != address(0), "token does not exist");
+        return _tokenApproval[tokenId];
     }
 
     function transferFrom(address from, address to, uint256 tokenId) public {
         // TODO: please add your implementaiton here
+        address owner = ownerOf(tokenId);
+        require(msg.sender == owner ||
+        _tokenApproval[tokenId] == msg.sender ||
+        _operatorApproval[owner][msg.sender], "not owner nor approved");
+
+        _transfer(owner, from, to, tokenId);
+    }
+
+    function _transfer(address owner, address from, address to, uint256 tokenId) private{
+        require(from == owner, "not owner");
+        require(to != address(0), "the address of receiver is 0");
+        _tokenApproval[tokenId] = to;
+        emit Approval(owner, to , tokenId);
+
+        _balances[from] -= 1;
+        _balances[to] += 1;
+        _owner[tokenId] = to;
+
+        emit Transfer(from, to , tokenId);
     }
 
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) public {
         // TODO: please add your implementaiton here
+        address owner = ownerOf(tokenId);
+        require(msg.sender == owner ||
+        _tokenApproval[tokenId] == msg.sender ||
+        _operatorApproval[owner][msg.sender],"not owner nor approved");
+
+        _transfer(owner, from, to, tokenId);
+        require(to.code.length == 0 || IERC721TokenReceiver(to).onERC721Received(msg.sender, from, tokenId, data) == IERC721TokenReceiver.onERC721Received.selector, "unsafe recipient");
+        
     }
 
     function safeTransferFrom(address from, address to, uint256 tokenId) public {
         // TODO: please add your implementaiton here
+        address owner = ownerOf(tokenId);
+        require(msg.sender == owner ||
+        _tokenApproval[tokenId] == msg.sender ||
+        _operatorApproval[owner][msg.sender],"not owner nor approved");
+
+        _transfer(owner, from, to, tokenId);
+        require(to.code.length == 0 || IERC721TokenReceiver(to).onERC721Received(msg.sender, from, tokenId, "") == IERC721TokenReceiver.onERC721Received.selector, "unsafe recipient");
     }
 }
